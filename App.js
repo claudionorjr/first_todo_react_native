c8eb9b78b43a2767264087578553523022bb0bad
Claudionor Silva <claudionor.junior1994@gmail.com>
2020-08-25 15:27:05 -0300

    Implementado Redux Persist

--- App.js
+++ App.js
@@ -2,16 +2,19 @@ import React from 'react'
 import reducer from './reducer'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import configureStore from './configureStore'
 import Task from './Task'
 
-
-const store = createStore(reducer)
+const {store, persistor} = configureStore()
 
 export default App = (props) => {
   
     return (
         <Provider store={store}>
-        <Task />
+            <PersistGate loading={null} persistor={persistor}>
+                <Task />
+            </PersistGate>
         </Provider>
     )
 }
